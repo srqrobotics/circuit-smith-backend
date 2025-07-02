5cfc22c75883eb46801c04c023f8ed9d33f7e300
Pasindu Vihangana <pasi1028@gmail.com>
2025-07-02 17:46:28 +0530

    strict -> none

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,7 +32,7 @@ const userController = {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
@@ -101,7 +101,7 @@ const userController = {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
