46031e31f9ba2fb6e04fdd6a5daa72a96b459005
Pasindu Vihangana <pasi1028@gmail.com>
2025-07-02 11:13:16 +0530

    debug log added

--- server.js
+++ server.js
@@ -16,4 +16,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("SESSION_SECRET at runtime:", process.env.SESSION_SECRET);
 });
